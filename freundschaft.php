9f3c29b473477c8bdba0f877f6ba78f064338607
fumikito <guy@hametuha.com>
2014-12-12 18:45:28 +0900

    Initial commit

---
+++ freundschaft.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Freundschaft
+Plugin URI: https://github.com/hametuha/freundschaft
+Description: Follow
+Author: Hametuha inc.
+Version: 1.0
+Author URI: http://hametuha.co.jp
+License: GPL2 or Later
+*/
